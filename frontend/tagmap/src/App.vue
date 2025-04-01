dbef213cdae9dca16e9f1f88120fa0b8d6cde39a
eldertek <andrelauret@icloud.com>
2025-04-01 23:51:39 +0200

    main: Updated

--- frontend/tagmap/src/App.vue
+++ frontend/tagmap/src/App.vue
@@ -324,39 +324,9 @@ watchEffect(() => {
                       d="M13 10V3L4 14h7v7l9-11h-7z" />
               </svg>
             </button>
-            <div class="flex md:hidden">
-              <button
-                @click="showMobileMenu = !showMobileMenu"
-                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500"
-              >
-                <span class="sr-only">Ouvrir le menu</span>
-                <svg
-                  class="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    v-if="!showMobileMenu"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                  <path
-                    v-else
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-            <!-- Dynamic Island Notification System -->
+
+            <!-- Notifications -->
             <div class="relative">
-              <!-- Bouton de cloche avec animation -->
               <button
                 ref="bellButtonRef"
                 @click="showNotificationsMenu = !showNotificationsMenu"
@@ -374,56 +344,7 @@ watchEffect(() => {
                 </svg>
               </button>
               
-              <!-- Dynamic Island Notification -->
-              <Transition name="island">
-                <div 
-                  v-if="activeNotification"
-                  class="dynamic-island fixed z-[9999] shadow-xl"
-                  :class="[
-                    isExpanded ? 'island-expanded' : 'island-pill',
-                    activeNotification.type === 'success' ? 'bg-green-500' : 
-                    activeNotification.type === 'error' ? 'bg-red-500' : 
-                    activeNotification.type === 'warning' ? 'bg-yellow-500' : 'bg-blue-500'
-                  ]"
-                  :style="{
-                    top: islandPositionTop,
-                    right: islandPositionRight,
-                    transform: 'translateZ(0)'
-                  }"
-                  @click="dismissActiveNotification"
-                >
-                  <div class="flex items-center h-full px-3 motion-reduce:transition-none">
-                    <!-- Icon based on notification type -->
-                    <div class="flex-shrink-0">
-                      <!-- Success icon -->
-                      <svg v-if="activeNotification.type === 'success'" class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                      </svg>
-                      <!-- Error icon -->
-                      <svg v-else-if="activeNotification.type === 'error'" class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
-                      </svg>
-                      <!-- Warning icon -->
-                      <svg v-else-if="activeNotification.type === 'warning'" class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
-                      </svg>
-                      <!-- Info icon -->
-                      <svg v-else class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
-                      </svg>
-                    </div>
-                    
-                    <!-- Message (visible only when expanded) -->
-                    <Transition name="message" :duration="{ enter: 200, leave: 150 }">
-                      <div v-if="isExpanded" class="ml-2 text-white font-medium message-container">
-                        {{ activeNotification.message }}
-                      </div>
-                    </Transition>
-                  </div>
-                </div>
-              </Transition>
-              
-              <!-- Dropdown notifications menu (still available when clicking on bell) -->
+              <!-- Menu notifications -->
               <div 
                 v-if="showNotificationsMenu" 
                 class="absolute right-0 mt-2 w-80 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-[3000]"
@@ -482,7 +403,8 @@ watchEffect(() => {
                 </div>
               </div>
             </div>
-            
+
+            <!-- Menu profil -->
             <div class="relative">
               <button
                 @click="showProfileMenu = !showProfileMenu"
@@ -498,6 +420,7 @@ watchEffect(() => {
                   :alt="userName"
                 />
               </button>
+              <!-- Menu profil déroulant -->
               <div
                 v-if="showProfileMenu"
                 class="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-[3000]"
@@ -525,6 +448,7 @@ watchEffect(() => {
             </div>
           </div>
         </div>
+
         <!-- Menu mobile -->
         <div
           v-if="showMobileMenu"
