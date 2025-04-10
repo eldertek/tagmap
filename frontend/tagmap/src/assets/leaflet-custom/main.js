26724d3dda61f3035fc21b15532fc8b071ef07bb
eldertek <andrelauret@icloud.com>
2025-04-10 14:05:50 +0200

    main: Updated

--- frontend/tagmap/src/assets/leaflet-custom/main.js
+++
@@ -1,7 +0,0 @@
-// Fichier principal pour l'importation de Leaflet personnalisé
-import './leaflet.css';
-import L from './leaflet.js';
-
-// Exporter Leaflet
-export default L;
-export * from './leaflet.js';
